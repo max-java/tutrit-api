748d0aaf611ba2cdf96113acde47e658ab1a3b85
max-java <6666350@gmail.com>
2021-02-25 13:29:46 +0300

    initial commit

---
+++ src/main/java/by/jrr/tutritapi/TutritapiApplication.java
@@ -0,0 +1,13 @@
+package by.jrr.tutritapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutritapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutritapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/jrr/tutritapi/TutritapiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.tutritapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutritapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
