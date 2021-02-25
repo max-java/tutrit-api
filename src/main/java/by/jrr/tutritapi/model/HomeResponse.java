78fc5ad4134965e8c28be891abe9b8351c92d057
max-java <6666350@gmail.com>
2021-02-25 15:09:58 +0300

    minor updates to test controller data

--- src/main/java/by/jrr/tutritapi/model/HomeResponse.java
+++ src/main/java/by/jrr/tutritapi/model/HomeResponse.java
@@ -3,7 +3,6 @@ package by.jrr.tutritapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Data
 @AllArgsConstructor
